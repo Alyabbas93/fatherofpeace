3181398961db191723267251907e1a22502c645b
shahzadakram786 <alyakram786@gmail.com>
2025-04-12 18:31:02 +0500

    feed: accepting the request and adding in friends list done

--- src/components/social/friendsContent.js
+++ src/components/social/friendsContent.js
@@ -2,11 +2,11 @@ import React from "react";
 import FriendList from "./friendList";
 import PeopleUmN from "./peopleUmN";
 
-const FriendsContent = ({dummyFriends ,dummyFriends2}) => {
+const FriendsContent = ({dummyFriends ,dummyFriends2 , acceptedFriends = []}) => {
   return (
     <div className="w-full flex  flex-col gap-8">
         
-      <FriendList dummyFriends={dummyFriends} />
+      <FriendList dummyFriends={dummyFriends} acceptedFriends={acceptedFriends} />
       <PeopleUmN dummyFriends2={dummyFriends2}/>
     </div>
   );
